6f64398d676b8435dfab022a7cf966ae460634ac
Moguhmand <aksel.ethelberg@gmail.com>
2024-04-30 09:42:45 +0200

    idfk

--- App.tsx
+++ App.tsx
@@ -3,31 +3,31 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './app/screens/Login';
-import Details from './app/screens/Details';
-import List from './app/screens/List';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import { FIREBASE_AUTH } from './firebaseConfig';
+import InsideLayout from './app/InsideLayout';
+
 
 const Stack = createNativeStackNavigator();
 
-const InsideStack = createNativeStackNavigator();
+// const InsideStack = createNativeStackNavigator();
 
-function InsideLayout() {
-  return (
-    <InsideStack.Navigator>
-      <InsideStack.Screen name='My todos' component={List} />
-      <InsideStack.Screen name='Details' component={Details} />
-    </InsideStack.Navigator>
-  )
-}
+// function InsideLayout() {
+//   return (
+//     <InsideStack.Navigator>
+//       <InsideStack.Screen name='Home' component={Home} />
+//       <InsideStack.Screen name='Details' component={Details} />
+//     </InsideStack.Navigator>
+//   )
+// }
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log('user', user);
+      //console.log('user', user);
       setUser(user);
     });
   }, []);
@@ -36,7 +36,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
         {user ? (
-          <Stack.Screen name='Inside' component={InsideLayout} options={{ headerShown: false }}/>
+          <Stack.Screen name='Inside' component={InsideLayout} options={{ headerShown: true }}/>
         ) : (
           <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}/>
         )}
